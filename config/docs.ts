073160b8f6530d68042273e4cc57bbf430104981
hanssn <hans@team-xyz.com>
2024-03-22 13:48:18 +0700

    init

---
+++ config/docs.ts
@@ -0,0 +1,52 @@
+import { MainNavItem, SidebarNavItem } from "types/nav"
+
+interface DocsConfig {
+  mainNav: MainNavItem[]
+  sidebarNav: SidebarNavItem[]
+}
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs",
+    },
+    {
+      title: "GitHub",
+      href: "https://github.com/shadcn/ui",
+      external: true,
+    },
+    {
+      title: "Twitter",
+      href: "https://twitter.com/shadcn",
+      external: true,
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs",
+          items: [],
+        },
+        {
+          title: "Changelog",
+          href: "/docs/changelog",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Components",
+      items: [
+        {
+          title: "Accordion",
+          href: "/docs/components/accordion",
+          items: [],
+        },
+      ],
+    },
+  ],
+}
---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import { createContentlayerPlugin } from "next-contentlayer"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+}
+
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+})
+
+export default withContentlayer(nextConfig)
